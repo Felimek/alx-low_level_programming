d7f398e4442c76097c51788d983d12e555ffe08e
Felimek <felimek@yahoo.com>
2022-10-24 11:50:32 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * * reset_to_98-update the value of nto 98
+ *
+ * @n:input
+ *
+ * Return:n
+ *
+ */
+
+void reset_to_98(int *n)
+
+{
+
+*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * * _putchar.c - write character c to stdout
+ *
+ * @c:char to be printed
+ *
+ * return:On success 1
+ *
+ */
+
+int _putchar.c(char c)
+
+{
+return (write1, &c, 1);
+
+}
