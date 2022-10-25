8621589b12d5777ee79ee78fd65c28a2b012e6da
Felimek <felimek@yahoo.com>
2022-10-25 09:23:43 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
-  * main - entry point
-  * Description: creates a random key for program 101_crackme
-  * Return: always 0
-  */
+ * main - generates keygen.
+ * Return: 0
+ */
 int main(void)
 {
-	    int sum, i, r;
+	int i = 0, j = 0;
+	time_t t;
 
-		char decode[27] = "abcdefghijklmnopqrstuvwxyz";
-		char key[30];
-
-		sum = 0;
-		i = 0;
-
-		srand(time(NULL));
-
-		while (sum < 2772)
-		{
-			r = rand() % 10;
-			key[i] = decode[r];
-			sum += key[i];
-			i++;
-		}
-		r = 2772 - sum;
-		key[i] = r;
-		printf("%s\n",  key);
-		return (0);
+	srand((unsigned int) time(&t));
+	while (j < 2772)
+	{
+		i = rand() % 128;
+		if ((j + i) > 2772)
+			break;
+		j = j + i;
+		printf("%c", i);
+	}
+	printf("%c\n", (2772 - j));
+	return (0);
 }
