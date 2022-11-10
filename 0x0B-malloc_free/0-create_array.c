ae50b5da88214616c86c8f0693312e142a539986
Felimek <felimek@yahoo.com>
2022-11-10 12:53:43 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,9 +25,8 @@ char *create_array(unsigned int size, char c)
 		char *s;
 
 			unsigned int i;
-
-		if (size == 0)
-				return (NULL);
+if (size == 0)
+	return (NULL);
 			s = malloc(sizeof(c) * size);
 			if (s == NULL)
 			return (NULL);
