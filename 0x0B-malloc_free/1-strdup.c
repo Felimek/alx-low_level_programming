0fdf4f1cdfee559c7f105187ab4cd9741e919f1d
Felimek <felimek@yahoo.com>
2022-11-10 12:47:23 -0800

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,8 +5,7 @@
 
 
 /**
- *
- *  * _strdup - points to new mem
+ * * _strdup - points to new mem
  *
  *   * @str: param
  *
@@ -23,27 +22,18 @@ char *_strdup(char *str)
 		char *s;
 
 			int i, j;
-
-
 if (str == NULL)
-
-							return (NULL);
-
+return (NULL);
 
 
 					for (i = 0; str[i] != '\0'; i++)
 						;
 
 				s = (char *)malloc(sizeof(char) * (i + 1));
-
-							if (s == NULL)
-
-										return (NULL);
-
-
-						for (j = 0; j <= i; j++)
-
-											s[j] = str[j];
+				if (s == NULL)
+					return (NULL);
+				for (j = 0; j <= i; j++)
+					s[j] = str[j];
 						return (s);
 
 }
